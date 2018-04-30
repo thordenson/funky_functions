7c6dfc76b2d3a079f4afd0b747b15986e5535133
Thor Denson <thordenson@Thors-MacBook-Pro.local>
2018-04-30 12:26:32 -0400

    establishing reverse function

---
+++ reverse.py
@@ -0,0 +1,11 @@
+
+def reverse(): 
+    original = "here i am"
+    new_text = ""
+
+    length = len(original) - 1
+    for i in range(length + 1):
+        return(length - i)
+    new_text = new_text + original[length - i]
+
+    return new_text
